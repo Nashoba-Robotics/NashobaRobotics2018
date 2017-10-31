2d9ae3c9a058bb9aa431c4aa245d4734c1ea5e8d
bf2799 <astudent@cinderhippo.com>
2017-10-31 15:25:47 -0400

    Removing 2018TestBot

--- 2018TestBot/src/edu/nr/robotics/subsystems/EnabledSybsystems.java
+++
@@ -1,8 +0,0 @@
-package edu.nr.robotics.subsystems;
-
-public class EnabledSybsystems {
-	
-	public static boolean DRIVE_ENABLED = true;
-	public static boolean DUMB_DRIVE_ENABLED = false;
-
-}
--- 2018TestBot/src/main/java/edu/nr/robotics/subsystems/EnabledSybsystems.java
+++
@@ -1,8 +0,0 @@
-package edu.nr.robotics.subsystems;
-
-public class EnabledSybsystems {
-	
-	public static boolean DRIVE_ENABLED = false;
-	public static boolean DUMB_DRIVE_ENABLED = true;
-
-}
