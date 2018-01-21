34448a1dd8c1bdb7d2869e8f468e5615643ff28c
bf2799 <b2k16@cinderhippo.com>
2018-01-21 18:04:36 -0500

    adding motion profiling commands into auto routes

---
+++ 2018RoboChiefs/src/edu/nr/robotics/auton/autoroutes/ScaleToBlockProfilingCommand.java
@@ -0,0 +1,11 @@
+package edu.nr.robotics.auton.autoroutes;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class ScaleToBlockProfilingCommand extends CommandGroup {
+	
+	public ScaleToBlockProfilingCommand(int block) {
+		
+	}
+
+}
--- 2018RoboChiefs/src/edu/nr/robotics/auton/autoroutes/StartPosFarRightToScaleLeftProfilingCommand.java
+++ 2018RoboChiefs/src/edu/nr/robotics/auton/autoroutes/StartPosFarRightToScaleLeftProfilingCommand.java
@@ -1,10 +1,21 @@
 package edu.nr.robotics.auton.autoroutes;
 
+import edu.nr.lib.units.Angle;
+import edu.nr.lib.units.Distance;
+import edu.nr.robotics.subsystems.drive.Drive;
+import edu.nr.robotics.subsystems.drive.EnableOneDMotionProfile;
+import edu.nr.robotics.subsystems.drive.TurnPIDCommand;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class StartPosFarRightToScaleLeftProfilingCommand extends CommandGroup {
 
 	public StartPosFarRightToScaleLeftProfilingCommand() {
+		//TODO: Fill in values
+		addSequential(new EnableOneDMotionProfile(Distance.ZERO, Distance.ZERO, Drive.PROFILE_DRIVE_PERCENT, Drive.ACCEL_PERCENT));
+		
+		addSequential(new EnableOneDMotionProfile(Distance.ZERO, Distance.ZERO, Drive.PROFILE_DRIVE_PERCENT, Drive.ACCEL_PERCENT));
+		
+		addSequential(new EnableOneDMotionProfile(Distance.ZERO, Distance.ZERO, Drive.PROFILE_DRIVE_PERCENT, Drive.ACCEL_PERCENT));
 		
 	}
 	
