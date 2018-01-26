e84164447e5d0fae1ef358a515c573c0b9ccc184
Ben Fox <b2k16@cinderhippo.com>
2018-01-26 17:39:01 -0500

    a couple small things

--- 2018RoboChiefs/src/edu/nr/robotics/subsystems/sensors/EnabledSensors.java
+++ 2018RoboChiefs/src/edu/nr/robotics/subsystems/sensors/EnabledSensors.java
@@ -12,11 +12,8 @@ public class EnabledSensors {
 	public static DigitalInput cubeHandlerSensor = new DigitalInput(RobotMap.CUBE_HANDLER_SENSOR_PORT);
 	public static DigitalInput floorSensor = new DigitalInput(RobotMap.FLOOR_SENSOR_PORT);
 	
-	public static boolean intakeSensorEnabled = false;
-	public static boolean elevatorSensorEnabled = false;
 	public static boolean limelightEnabled = false;
 	public static boolean portalSensorEnabled = false;
-	public static boolean cubeHandlerSensorEnabled = false; //TODO: See what other sensors we need
 	public static boolean floorSensorEnabled = false;
 	
 	public static boolean floorTapeSeen = false;
