473e3ba62f0213acaf4a983847a90a0c1873e87e
nathanielcrosby <b2k16@cinderhippo.com>
2018-03-29 17:18:35 -0400

    added a single parenth to one of the random autos

--- 2018RoboChiefs/src/edu/nr/robotics/auton/autoroutes/PlatformZoneSwitchLeftToBlockProfilingCommand.java
+++ 2018RoboChiefs/src/edu/nr/robotics/auton/autoroutes/PlatformZoneSwitchLeftToBlockProfilingCommand.java
@@ -35,7 +35,7 @@ public class PlatformZoneSwitchLeftToBlockProfilingCommand extends CommandGroup 
 				addParallel(new IntakeDeployCommand());
 				
 				addSequential(new EnableMotionProfile(
-						(FieldMeasurements.BASELINE_TO_PLATFORM_ZONE_X.sub(FieldMeasurements.BASELINE_TO_MID_SWITCH_X).negate(),
+						(FieldMeasurements.BASELINE_TO_PLATFORM_ZONE_X.sub(FieldMeasurements.BASELINE_TO_MID_SWITCH_X).negate()),
 						Distance.ZERO, Drive.PROFILE_DRIVE_PERCENT, Drive.ACCEL_PERCENT));
 				
 			}
