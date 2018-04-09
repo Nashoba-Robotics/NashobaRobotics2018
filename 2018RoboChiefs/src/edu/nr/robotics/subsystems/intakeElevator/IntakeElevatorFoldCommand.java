9c5fbf5bf798d7874194e9f329cf9b4b91c4160c
bf2799 <b2k16@cinderhippo.com>
2018-04-09 17:02:45 -0400

    intake folding is timed

--- 2018RoboChiefs/src/edu/nr/robotics/subsystems/intakeElevator/IntakeElevatorFoldCommand.java
+++ 2018RoboChiefs/src/edu/nr/robotics/subsystems/intakeElevator/IntakeElevatorFoldCommand.java
@@ -1,8 +1,13 @@
 package edu.nr.robotics.subsystems.intakeElevator;
 
 import edu.nr.lib.commandbased.NRCommand;
+import edu.wpi.first.wpilibj.Timer;
 
 public class IntakeElevatorFoldCommand extends NRCommand {
+	
+	private double initFoldTime = 0;
+	private boolean folding = false;
+	private boolean isFolded = false;
 	
 	public IntakeElevatorFoldCommand() {
 		super(IntakeElevator.getInstance());
@@ -12,11 +17,29 @@ public class IntakeElevatorFoldCommand extends NRCommand {
 	protected void onStart() {
 		IntakeElevator.getInstance().setMotorSpeedPercent(0.6);
 		IntakeElevator.intakeFolded = true;
+		initFoldTime = 0;
+		folding = false;
+		isFolded = false;
+	}
+	
+	@Override
+	protected void onExecute() {
+		if (IntakeElevator.getInstance().getCurrent() >= IntakeElevator.FOLD_CURRENT_SPIKE) {
+			if (!folding) {
+				folding = true;
+				initFoldTime = Timer.getFPGATimestamp();
+				
+			} else {
+				if ((Timer.getFPGATimestamp() - initFoldTime) * 1000 >= IntakeElevator.PEAK_CURRENT_DURATION_INTAKE_ELEVATOR) {
+					isFolded = true;
+				}
+			}
+		}
 	}
 	
 	@Override
 	protected boolean isFinishedNR() {
-		return IntakeElevator.getInstance().getCurrent() >= IntakeElevator.FOLD_CURRENT_SPIKE;
+		return isFolded;
 	}
 
 }
